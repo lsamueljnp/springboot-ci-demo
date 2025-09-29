9517c0427f66201bfa587b24b8a0ce21c42457f1
Libi Samuel <libi.samuel@gmail.com>
2025-09-29 12:20:25 -0500

    Added  new methods to Controller

--- src/main/java/com/example/hello_rest/HelloController.java
+++ src/main/java/com/example/hello_rest/HelloController.java
@@ -1,13 +1,20 @@
 package com.example.hello_rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello from Spring Boot on macOS M4!!";
+    public String sayHello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "Hello " + name + " from Spring Boot on macOS M4!!";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String sayHelloWithPath(@PathVariable String name) {
+        return "Hello " + name + " from Spring Boot on macOS M4!!";
     }
 }
